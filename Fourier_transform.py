9ec5a2fc2cd038a50f61541851c2ff6ee270d822
Sean Dillon <smdillon@mail.csuchico.edu>
2020-01-29 09:36:15 -0800

    created file to take fourier transform of data

---
+++ Fourier_transform.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 09:33:45 2020
+
+@author: seand
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
